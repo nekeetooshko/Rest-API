e74047cdef1f28c44c24735f2e3eec1be31abe0f
Nikita Filichev <richardplantagenetlh@gmail.com>
2025-03-13 16:46:51 +0300

    Добавил config.yaml, проинициализировал конфиг (вайпер) в main'e

--- cmd/main.go
+++ cmd/main.go
@@ -6,11 +6,17 @@ import (
 	"MaksJash/pkg/repository"
 	"MaksJash/pkg/service"
 	"log"
+
+	"github.com/spf13/viper"
 )
 
 // Запуск проги
 
 func main() {
+
+	if err := initConfig(); err != nil {
+		log.Fatalf("Error with config initialization: %s", err.Error()) // .Error() - приведение к строке
+	}
 
 	rep := repository.NewRepository()
 	srvc := service.NewService(rep)
@@ -18,8 +24,17 @@ func main() {
 
 	server := new(todo.Server)
 
-	if err := server.Run("9090", handler.InitRoutes()); err != nil {
-		log.Fatalf("Error while running the server: %s", err.Error()) // .Error() - приведение к строке
+	if err := server.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+		log.Fatalf("Error while running the server: %s", err.Error())
 	}
 
+}
+
+// Инициализация конфига
+func initConfig() error {
+
+	viper.AddConfigPath("configs") // Указание директории, где будет искать файл конфига
+	viper.SetConfigName("config")  // Имя файла конфига, что нужно найти
+
+	return viper.ReadInConfig()
 }
